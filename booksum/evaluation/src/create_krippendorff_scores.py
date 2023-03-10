f57120a6d16329f51dd31547229528fc10c47714
Cody-San <cody_airey@icloud.com>
2023-03-10 15:47:33 +1300

    move to desktop

--- booksum/evaluation/src/create_krippendorff_scores.py
+++ booksum/evaluation/src/create_krippendorff_scores.py
@@ -24,29 +24,34 @@ def find__most_matched_source(dataset):
         "sparknotes"   : 0,
         "thebestnotes" : 0
     }
+    
+    x = "chapter" if dataset == "section" else "book"
 
-    f = open(pathlib.Path(f"../../alignments/{dataset}-level-summary-alignments/fixed_{dataset}_summaries_all_final.jsonl"), encoding='utf-8')
+    f = open(pathlib.Path(f"../../alignments/{x}-level-summary-alignments/fixed_{dataset}_summaries_all.jsonl"), encoding='utf-8')
 
     for line in f:
         summary = json.loads(line)
         number_of_matches[summary['source']] += 1
-    for e in number_of_matches:
-        print(e, number_of_matches[e])
-    source = [i for i in number_of_matches if number_of_matches[i] == max(number_of_matches.values())][0]
+    # for e in number_of_matches:
+    #     print(e, number_of_matches[e])
+    source = max(number_of_matches, key=number_of_matches.get)
+    # print(source)
     return source
 
 
 def find_related_lbl_files(dataset):
-    csv_folder_path = "../csv_results/booksum_summaries/fixed/line_by_line_{dataset}"
+    csv_folder_path = f"../csv_results/booksum_summaries/fixed/line_by_line_{dataset}"
     for (dirpath, dirnames, filenames) in os.walk(csv_folder_path):
+        # print(dirpath)
         for file in filenames:
-            if Path(file.suffix) == ".csv":
+            print(file)
+            if Path(file).suffix == ".csv":
                 if dataset == "book":
                     book_files.append(file)
                 elif dataset == "section":
                     section_files.append(file)
 
-def gather_book_summary_sentences(dataset, source):
+def gather_sentence_ids(dataset, source):
     """Creates a list of sentences depending on dataset given (book or section) using the most correlated/matched source
 
     Args:
@@ -59,56 +64,63 @@ def gather_book_summary_sentences(dataset, source):
     df = "NA"
     if dataset == "book":
         title = "Book Title"
-        df = pd.read_csv(f"../csv_results/booksum_summaries/fixed/line_by_line{dataset}/{book_files[0]}")
+        df = pd.read_csv(f"../csv_results/booksum_summaries/fixed/line_by_line_{dataset}/{book_files[0]}")
     elif dataset == "section":
         title = "Section Title"
-        df = pd.read_csv(f"../csv_results/booksum_summaries/fixed/line_by_line{dataset}/{section_files[0]}")
+        df = pd.read_csv(f"../csv_results/booksum_summaries/fixed/line_by_line_{dataset}/{section_files[0]}")
     else:
         print("bad dataset/scope given!", file=sys.stderr)
         sys.exit(1)
+        
+    # print(df.head())
 
     custom_ids = {} #silly but it works, ordered + hashed
     for index, row in df.iterrows():
         if row['Reference Source'] == source:
             custom_id = row[title] + ", sentence-" + str(row['Reference Sentence Index'])
             custom_ids[custom_id] = custom_id
-        # if len(custom_ids) >= 10:
-        #     break
+        if len(custom_ids) >= 10:
+            break
 
-    # print("Doneish")
-
-    return custom_ids.keys()
+    return list(custom_ids.keys())
 
 
 def create_kappa_table(sentence_ids, source, infilename, dataset):
     #setup template for new df
     sources = ["bookwolf", "cliffnotes", "gradesaver", "novelguide", "pinkmonkey", "sparknotes", "thebestnotes"]
     newdf = pd.DataFrame(index = sentence_ids, columns=sources)
-    print(newdf.head)
+
+    filetitle = "AHHH"
+    
+    title = "Section Title" if dataset == "section" else "Book Title"
     
     #Grab title from lbl file to use in new krippendorff table name
     filename_split = infilename.split("-")
+    print(filename_split)
     for i, split in enumerate(filename_split):
-        if split == "lbl":
+        if split == "lbl.csv":
             filetitle = filename_split[i-1]
 
     #use lbl file as df
-    filepath = "../csv_results/booksum_summaries/fixed/line_by_line{dataset}"
+    filepath = f"../csv_results/booksum_summaries/fixed/line_by_line_{dataset}"
     df = pd.read_csv(f"{filepath}/{infilename}")
+    f1_title = df.columns[5]
 
     #grab max f1 for any unique sentence for ref source
     for index, row in df.iterrows():
         if row['Reference Source'] == source:
-            custom_id = row['Section Title'] + ", sentence-" + str(row['Reference Sentence Index'])
+            custom_id = row[title] + ", sentence-" + str(row['Reference Sentence Index'])
             if np.isnan(newdf.loc[custom_id, row["Hypothesis Source"]]):
-                newdf.loc[custom_id, row["Hypothesis Source"]] = row['rouge-1nscore']
-            newdf.loc[custom_id, row["Hypothesis Source"]] = max(newdf.loc[custom_id, row["Hypothesis Source"]], row['rouge-1nscore'])
+                newdf.loc[custom_id, row["Hypothesis Source"]] = row[f1_title]
+            newdf.loc[custom_id, row["Hypothesis Source"]] = max(newdf.loc[custom_id, row["Hypothesis Source"]], row[f1_title])
 
     #fill out values that have no scores
     newdf = newdf.fillna(value= np.nan)
 
     outfilename = f"{source}-{filetitle}-sentence-max-scores.csv"
-    newdf.to_csv(f"../csv_results/krippendorff/{dataset}/{outfilename}", na_rep=np.nan, index=False)
+    newdf.to_csv(f"../csv_results/krippendorff/{dataset}/{outfilename}", na_rep=np.nan)
+    
+    print(newdf.head())
 
 
 #Not actually sure this is good / works. uses set value of .2; doesn't work for all metrics 
@@ -131,3 +143,19 @@ def adjust_kappa_tables_floor_ceil(dataset):
             
             dfin.to_csv(f"{dirpath}/adjusted_results/{file}")
     
+    
+#why didin't I just use more global variables.
+def main():
+    
+    for dataset in ["section", "book"]:
+        source = find__most_matched_source(dataset)
+        find_related_lbl_files(dataset)
+        
+        ids = gather_sentence_ids(dataset, source)
+
+        files = section_files if dataset == "section" else book_files
+        for file in files:
+            create_kappa_table(ids, source, file, dataset)
+
+if __name__ == "__main__":
+    main()
